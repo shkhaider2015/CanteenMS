9bfb6e947a5e28d8eb9a265c1e35e4c1be37cd92
Haider <shkhaider2015@gmail.com>
2020-04-12 13:10:50 +0500

    login and register UI complete

--- app/src/main/java/com/example/canteenms/Activities/Login.java
+++ app/src/main/java/com/example/canteenms/Activities/Login.java
@@ -3,14 +3,48 @@ package com.example.canteenms.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.canteenms.R;
 
-public class Login extends AppCompatActivity {
+public class Login extends AppCompatActivity  implements View.OnClickListener {
+
+    private EditText mEmail, mPassword;
+    private Button mLogin;
+    private TextView mNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    private void init()
+    {
+        mEmail = findViewById(R.id.login_email);
+        mPassword = findViewById(R.id.login_password);
+        mLogin = findViewById(R.id.login_log_in);
+        mNavigation = findViewById(R.id.login_navigation);
+
+        mLogin.setOnClickListener(this);
+        mNavigation.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.login_log_in:
+                // login logic
+                break;
+            case R.id.login_navigation:
+                // navigation Logic
+                break;
+        }
+
     }
 }
--- app/src/main/java/com/example/canteenms/Activities/Register.java
+++ app/src/main/java/com/example/canteenms/Activities/Register.java
@@ -3,14 +3,58 @@ package com.example.canteenms.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.canteenms.R;
 
-public class Register extends AppCompatActivity {
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class Register extends AppCompatActivity implements View.OnClickListener {
+
+    private CircleImageView mProfileImageView;
+    private EditText mFullName, mEmail, mPassword, mConfirmPassword;
+    private TextView mNavigationText;
+    private Button mSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        init();
+    }
+
+    private void init()
+    {
+        mProfileImageView = findViewById(R.id.register_image);
+        mFullName = findViewById(R.id.register_name);
+        mEmail = findViewById(R.id.register_email);
+        mPassword = findViewById(R.id.register_password);
+        mConfirmPassword = findViewById(R.id.register_confirm_password);
+        mNavigationText = findViewById(R.id.register_navigation);
+        mSignUp = findViewById(R.id.register_sign_up);
+
+        mProfileImageView.setOnClickListener(this);
+        mSignUp.setOnClickListener(this);
+        mNavigationText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.register_image:
+                // image logic
+                break;
+            case R.id.register_sign_up:
+                // sign up logic here
+                break;
+            case R.id.register_navigation:
+                // Navigation Logic
+                break;
+        }
     }
 }
