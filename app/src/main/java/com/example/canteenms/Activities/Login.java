7a317f85a89fce674237d7019357b5ec9a08ae27
Haider <shkhaider2015@gmail.com>
2020-04-24 03:26:49 +0500

    login and register UI complete

--- app/src/main/java/com/example/canteenms/Activities/Login.java
+++ app/src/main/java/com/example/canteenms/Activities/Login.java
@@ -4,18 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.canteenms.R;
+
 
 public class Login extends AppCompatActivity  implements View.OnClickListener {
 
     private EditText mEmail, mPassword;
     private Button mLogin;
     private TextView mNavigation;
+    private ProgressBar mProgressbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,7 @@ public class Login extends AppCompatActivity  implements View.OnClickListener {
         mPassword = findViewById(R.id.login_password);
         mLogin = findViewById(R.id.login_log_in);
         mNavigation = findViewById(R.id.login_navigation);
+        mProgressbar = findViewById(R.id.login_progress_bar);
 
         mLogin.setOnClickListener(this);
         mNavigation.setOnClickListener(this);
@@ -42,8 +50,7 @@ public class Login extends AppCompatActivity  implements View.OnClickListener {
         {
             case R.id.login_log_in:
                 // login logic
-                startActivity(new Intent(Login.this, Home.class));
-                finish();
+                login();
                 break;
             case R.id.login_navigation:
                 // navigation Logic
@@ -52,6 +59,48 @@ public class Login extends AppCompatActivity  implements View.OnClickListener {
         }
 
     }
-    //commit one
-    //commit two
+
+    private void login()
+    {
+        String email, password;
+        email = mEmail.getText().toString();
+        password = mPassword.getText().toString();
+
+        if (email.isEmpty())
+        {
+            mEmail.setError("Please Enter Your Email");
+            mEmail.requestFocus();
+            return;
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches())
+        {
+            mEmail.setError("Email Is Not Valid");
+            mEmail.requestFocus();
+            return;
+        }
+        if (password.isEmpty())
+        {
+            mPassword.setError("Please Enter Your Password");
+            mPassword.requestFocus();
+            return;
+        }
+        if (password.length() <= 6)
+        {
+            mPassword.setError("Password Is Not Correct");
+            mPassword.requestFocus();
+            return;
+        }
+
+        Toast.makeText(getApplicationContext(),
+                "Login Correct",
+                Toast.LENGTH_SHORT)
+                .show();
+
+        mProgressbar.setVisibility(View.VISIBLE);
+
+//        startActivity(new Intent(Login.this, Home.class));
+//        finish();
+    }
+
+
 }
