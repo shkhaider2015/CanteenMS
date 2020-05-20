7764485f02fa66d9169e2ab6f89a66250ad297fc
Haider <shkhaider2015@gmail.com>
2020-05-21 00:28:46 +0500

    chat activity convert to chat fragment and chat activity deleted

--- app/src/main/java/com/example/canteenms/Utilities/Calculation.java
+++ app/src/main/java/com/example/canteenms/Utilities/Calculation.java
@@ -1,8 +1,11 @@
 package com.example.canteenms.Utilities;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
 
 public class Calculation {
 
@@ -15,11 +18,24 @@ public class Calculation {
     public static String getDate(long milliSeconds, String dateFormat)
     {
         // Create a DateFormatter object for displaying date in specified format.
-        SimpleDateFormat formatter = new SimpleDateFormat(dateFormat, Locale.UK);
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MMM/yyyy", Locale.UK);
 
         // Create a calendar object that will convert the date and time value in milliseconds to date.
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(milliSeconds);
+
+        
+
         return formatter.format(calendar.getTime());
+    }
+
+    public static String getTime(long milliSeconds)
+    {
+        long minuts = TimeUnit.MILLISECONDS.toMinutes(milliSeconds);
+        long hourse = TimeUnit.MILLISECONDS.toHours(milliSeconds);
+        long day = TimeUnit.MILLISECONDS.toDays(milliSeconds);
+
+
+        return "";
     }
 }
